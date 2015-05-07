86d51004a91a820f681b7b03ef86ac2f8603fbe7
gexplorer <gexplorer8@gmail.com>
2015-05-07 11:09:38 +0200

    Added several properties to the torrent model

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,12 +1,15 @@
 angular.module('starter.controllers', [])
 
 .controller('TorrentsCtrl', function($scope, Torrents) {
-  $scope.torrents = Torrents.all();
-  $scope.remove = function(torrent) {
-    Torrents.remove(torrent);
-  }
+	$scope.torrents = Torrents.all();
+	$scope.download = function(torrent){
+		Torrents.download(torrent);
+	}
 })
 
 .controller('TorrentDetailCtrl', function($scope, $stateParams, Torrents) {
-  $scope.torrent = Torrents.get($stateParams.torrentId);
+	$scope.torrent = Torrents.get($stateParams.torrentId);
+	$scope.download = function(torrent){
+		Torrents.download(torrent);
+	}
 });
--- www/js/services.js
+++ www/js/services.js
@@ -1,34 +1,49 @@
-angular.module('starter.services', [])
+angular.module('starter.services', ['ionic'])
 
-.factory('Torrents', function() {
+.factory('Torrents', function($ionicPopup) {
   // Might use a resource here that returns a JSON array
 
   // Some fake testing data
   var torrents = [{
     id: 0,
     name: 'Game of Thrones S04E04 HDTV x264-KILLERS[ettv]',
-    lastText: 'Size: 351 MB   Age: 1 year   Seed: 820   Leech: 15',
-    face: 'http://icons.iconarchive.com/icons/marcus-roberto/google-play/64/Google-Play-Movies-icon.png'
+    size: '351 MB',
+    age: '1 year',
+    seed: 820,
+    leech: 15,
+    icon: 'http://icons.iconarchive.com/icons/marcus-roberto/google-play/64/Google-Play-Movies-icon.png'
   }, {
     id: 1,
     name: 'Game of Thrones S04E04 720p HDTV x264-KILLERS [eztv]',
-    lastText: 'Size: 1.25 GB   Age: 1 year   Seed: 360   Leech: 18',
-    face: 'http://icons.iconarchive.com/icons/marcus-roberto/google-play/64/Google-Play-Movies-icon.png'
+    size: '1.25 GB',
+    age: '1 year',
+    seed: 360,
+    leech: 18,
+    icon: 'http://icons.iconarchive.com/icons/marcus-roberto/google-play/64/Google-Play-Movies-icon.png'
   },{
     id: 2,
     name: 'Game of Thrones S04E04 720p HDTV x264-KILLERS',
-    lastText: 'Size: 1.25 GB   Age: 1 year   Seed: 240   Leech: 3',
-    face: 'http://icons.iconarchive.com/icons/marcus-roberto/google-play/64/Google-Play-Movies-icon.png'
+    size: '1.25 GB',
+    age: '1 year',
+    seed: 240,
+    leech: 3,
+    icon: 'http://icons.iconarchive.com/icons/marcus-roberto/google-play/64/Google-Play-Movies-icon.png'
   }, {
     id: 3,
     name: 'Game of Thrones Season 4 S04E04 720p hdtv x264 MrLss',
-    lastText: 'Size: 386 MB   Age: 1 year   Seed: 121   Leech: 1',
-    face: 'http://icons.iconarchive.com/icons/marcus-roberto/google-play/64/Google-Play-Movies-icon.png'
+    size: '386 MB',
+    age: '1 year',
+    seed: 121,
+    leech: 1,
+    icon: 'http://icons.iconarchive.com/icons/marcus-roberto/google-play/64/Google-Play-Movies-icon.png'
   }, {
     id: 4,
     name: 'Game of Thrones S04E04 HDTV x264-KILLERS English Subtitle',
-    lastText: 'Size: 42 KB   Age: 1 year   Seed: 95   Leech: 21', 
-    face: 'https://cdn4.iconfinder.com/data/icons/gnome-desktop-icons-png/PNG/64/Gnome-Text-X-Generic-64.png'
+    size: '42 KB',
+    age: '1 year',
+    seed: 95,
+    leech: 21, 
+    icon: 'https://cdn4.iconfinder.com/data/icons/gnome-desktop-icons-png/PNG/64/Gnome-Text-X-Generic-64.png'
   }];
 
   return {
@@ -45,6 +60,19 @@ angular.module('starter.services', [])
         }
       }
       return null;
+    },
+    download: function(torrent) {
+      var confirmPopup = $ionicPopup.confirm({
+        title: 'Download',
+        template: 'Do you really want to download this magnet?'
+      });
+      confirmPopup.then(function(res) {
+        if(res) {
+          console.log('You do want');
+        } else {
+          console.log('You do not want');
+        }
+      });
     }
   };
 });
