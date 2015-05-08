23cc14ec275b070331d2900ec4052dc668bedda9
gexplorer <gexplorer8@gmail.com>
2015-05-08 10:50:03 +0200

    Added icons/colors for all the categories

--- www/js/services.js
+++ www/js/services.js
@@ -10,7 +10,7 @@ angular.module('cascade.services', [])
             age: '1 year',
             seed: 820,
             leech: 15,
-            icon: 'http://icons.iconarchive.com/icons/marcus-roberto/google-play/64/Google-Play-Movies-icon.png'
+            category: 'tv'
         }, {
             id: 1,
             name: 'Game of Thrones S04E04 720p HDTV x264-KILLERS [eztv]',
@@ -19,34 +19,61 @@ angular.module('cascade.services', [])
             age: '1 year',
             seed: 360,
             leech: 18,
-            icon: 'http://icons.iconarchive.com/icons/marcus-roberto/google-play/64/Google-Play-Movies-icon.png'
+            category: 'tv'
         }, {
             id: 2,
-            name: 'Game of Thrones S04E04 720p HDTV x264-KILLERS',
+            name: 'Game of Thrones - The Movie',
             magnet: 'magnet:?xt=urn:btih:B170691FE3B6AAEBA73F8EA122E85DED4AD2E1BF&dn=game+of+thrones+s04e04+hdtv+x264+killers+ettv&tr=http%3A%2F%2Ftracker.trackerfix.com%2Fannounce&tr=udp%3A%2F%2Fopen.demonii.com%3A1337',
-            size: '1.25 GB',
+            size: '4.2 GB',
+            age: '1 day',
+            seed: 905,
+            leech: 210,
+            category: 'movie'
+        }, {
+            id: 3,
+            name: 'Game of Thrones - Soundtrak',
+            magnet: 'magnet:?xt=urn:btih:B170691FE3B6AAEBA73F8EA122E85DED4AD2E1BF&dn=game+of+thrones+s04e04+hdtv+x264+killers+ettv&tr=http%3A%2F%2Ftracker.trackerfix.com%2Fannounce&tr=udp%3A%2F%2Fopen.demonii.com%3A1337',
+            size: '635 MB',
             age: '1 year',
             seed: 240,
             leech: 3,
-            icon: 'http://icons.iconarchive.com/icons/marcus-roberto/google-play/64/Google-Play-Movies-icon.png'
+            category: 'music'
         }, {
-            id: 3,
-            name: 'Game of Thrones Season 4 S04E04 720p hdtv x264 MrLss',
+            id: 4,
+            name: 'Game of Thrones - Book 1',
+            magnet: 'magnet:?xt=urn:btih:B170691FE3B6AAEBA73F8EA122E85DED4AD2E1BF&dn=game+of+thrones+s04e04+hdtv+x264+killers+ettv&tr=http%3A%2F%2Ftracker.trackerfix.com%2Fannounce&tr=udp%3A%2F%2Fopen.demonii.com%3A1337',
+            size: '420 KB',
+            age: '1 year',
+            seed: 95,
+            leech: 29,
+            category: 'book'
+        }, {
+            id: 5,
+            name: 'Game of Thrones - NDS Game',
             magnet: 'magnet:?xt=urn:btih:B170691FE3B6AAEBA73F8EA122E85DED4AD2E1BF&dn=game+of+thrones+s04e04+hdtv+x264+killers+ettv&tr=http%3A%2F%2Ftracker.trackerfix.com%2Fannounce&tr=udp%3A%2F%2Fopen.demonii.com%3A1337',
             size: '386 MB',
             age: '1 year',
-            seed: 121,
-            leech: 1,
-            icon: 'http://icons.iconarchive.com/icons/marcus-roberto/google-play/64/Google-Play-Movies-icon.png'
+            seed: 21,
+            leech: 15,
+            category: 'game'
         }, {
-            id: 4,
-            name: 'Game of Thrones S04E04 HDTV x264-KILLERS English Subtitle',
+            id: 6,
+            name: 'Game of Thrones Companion APP',
             magnet: 'magnet:?xt=urn:btih:B170691FE3B6AAEBA73F8EA122E85DED4AD2E1BF&dn=game+of+thrones+s04e04+hdtv+x264+killers+ettv&tr=http%3A%2F%2Ftracker.trackerfix.com%2Fannounce&tr=udp%3A%2F%2Fopen.demonii.com%3A1337',
-            size: '42 KB',
+            size: '213 MB',
             age: '1 year',
-            seed: 95,
-            leech: 21,
-            icon: 'https://cdn4.iconfinder.com/data/icons/gnome-desktop-icons-png/PNG/64/Gnome-Text-X-Generic-64.png'
+            seed: 45,
+            leech: 42,
+            category: 'app'
+        }, {
+            id: 7,
+            name: 'Game of Thrones - XXX Parody',
+            magnet: 'magnet:?xt=urn:btih:B170691FE3B6AAEBA73F8EA122E85DED4AD2E1BF&dn=game+of+thrones+s04e04+hdtv+x264+killers+ettv&tr=http%3A%2F%2Ftracker.trackerfix.com%2Fannounce&tr=udp%3A%2F%2Fopen.demonii.com%3A1337',
+            size: '678 MB',
+            age: '1 year',
+            seed: 866,
+            leech: 123,
+            category: 'xxx'
         }];
 
         return {
