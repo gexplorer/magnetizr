f55d9e28f0f69fe587ceb28c9eaa689eeedd205a
ipinto <ipinto@theinit.com>
2015-05-13 19:07:23 +0200

    Include Strike Search API and Cathegories matched

--- www/js/services.js
+++ www/js/services.js
@@ -32,30 +32,30 @@ angular.module('magnetizr.services', [])
         }
     })
 
-    .factory('Torrents', function () {
+    .factory('Torrents', function ($http) {
 
         function getColor(category) {
             var color = "light";
             switch (category) {
-                case "tv":
+                case "TV":
                     color = "positive";
                     break;
-                case "movie":
+                case "Movies":
                     color = "calm";
                     break;
-                case "book":
+                case "Books":
                     color = "balanced";
                     break;
-                case "music":
+                case "Music":
                     color = "royal";
                     break;
-                case "game":
+                case "Games":
                     color = "assertive";
                     break;
-                case "app":
+                case "Applications":
                     color = "dark";
                     break;
-                case "xxx":
+                case "XXX":
                     color = "stable";
             }
             return color;
@@ -64,119 +64,31 @@ angular.module('magnetizr.services', [])
         function getIcon(category) {
             var color = "paperclip";
             switch (category) {
-                case "tv":
+                case "TV":
                     color = "monitor";
                     break;
-                case "movie":
+                case "Movies":
                     color = "ios-videocam";
                     break;
-                case "book":
+                case "Books":
                     color = "ios-book";
                     break;
-                case "music":
+                case "Music":
                     color = "music-note";
                     break;
-                case "game":
+                case "Games":
                     color = "ios-game-controller-b";
                     break;
-                case "app":
+                case "Applications":
                     color = "gear-a";
                     break;
-                case "xxx":
+                case "XXX":
                     color = "alert-circled";
             }
             return color;
         }
 
-        var torrents = [{
-            id: 0,
-            name: 'Game of Thrones S04E04 HDTV x264-KILLERS[ettv]',
-            magnet: 'magnet:?xt=urn:btih:B170691FE3B6AAEBA73F8EA122E85DED4AD2E1BF&dn=game+of+thrones+s04e04+hdtv+x264+killers+ettv&tr=http%3A%2F%2Ftracker.trackerfix.com%2Fannounce&tr=udp%3A%2F%2Fopen.demonii.com%3A1337',
-            size: '351 MB',
-            age: '1 year',
-            seed: 820,
-            leech: 15,
-            category: 'tv',
-            color: getColor('tv'),
-            icon: getIcon('tv')
-        }, {
-            id: 1,
-            name: 'Game of Thrones S04E04 720p HDTV x264-KILLERS [eztv]',
-            magnet: 'magnet:?xt=urn:btih:B170691FE3B6AAEBA73F8EA122E85DED4AD2E1BF&dn=game+of+thrones+s04e04+hdtv+x264+killers+ettv&tr=http%3A%2F%2Ftracker.trackerfix.com%2Fannounce&tr=udp%3A%2F%2Fopen.demonii.com%3A1337',
-            size: '1.25 GB',
-            age: '1 year',
-            seed: 360,
-            leech: 18,
-            category: 'tv',
-            color: getColor('tv'),
-            icon: getIcon('tv')
-        }, {
-            id: 2,
-            name: 'Game of Thrones - The Movie',
-            magnet: 'magnet:?xt=urn:btih:B170691FE3B6AAEBA73F8EA122E85DED4AD2E1BF&dn=game+of+thrones+s04e04+hdtv+x264+killers+ettv&tr=http%3A%2F%2Ftracker.trackerfix.com%2Fannounce&tr=udp%3A%2F%2Fopen.demonii.com%3A1337',
-            size: '4.2 GB',
-            age: '1 day',
-            seed: 905,
-            leech: 210,
-            category: 'movie',
-            color: getColor('movie'),
-            icon: getIcon('movie')
-        }, {
-            id: 3,
-            name: 'Game of Thrones - Soundtrak',
-            magnet: 'magnet:?xt=urn:btih:B170691FE3B6AAEBA73F8EA122E85DED4AD2E1BF&dn=game+of+thrones+s04e04+hdtv+x264+killers+ettv&tr=http%3A%2F%2Ftracker.trackerfix.com%2Fannounce&tr=udp%3A%2F%2Fopen.demonii.com%3A1337',
-            size: '635 MB',
-            age: '1 year',
-            seed: 240,
-            leech: 3,
-            category: 'music',
-            color: getColor('music'),
-            icon: getIcon('music')
-        }, {
-            id: 4,
-            name: 'Game of Thrones - Book 1',
-            magnet: 'magnet:?xt=urn:btih:B170691FE3B6AAEBA73F8EA122E85DED4AD2E1BF&dn=game+of+thrones+s04e04+hdtv+x264+killers+ettv&tr=http%3A%2F%2Ftracker.trackerfix.com%2Fannounce&tr=udp%3A%2F%2Fopen.demonii.com%3A1337',
-            size: '420 KB',
-            age: '1 year',
-            seed: 95,
-            leech: 29,
-            category: 'book',
-            color: getColor('book'),
-            icon: getIcon('book')
-        }, {
-            id: 5,
-            name: 'Game of Thrones - NDS Game',
-            magnet: 'magnet:?xt=urn:btih:B170691FE3B6AAEBA73F8EA122E85DED4AD2E1BF&dn=game+of+thrones+s04e04+hdtv+x264+killers+ettv&tr=http%3A%2F%2Ftracker.trackerfix.com%2Fannounce&tr=udp%3A%2F%2Fopen.demonii.com%3A1337',
-            size: '386 MB',
-            age: '1 year',
-            seed: 21,
-            leech: 15,
-            category: 'game',
-            color: getColor('game'),
-            icon: getIcon('game')
-        }, {
-            id: 6,
-            name: 'Game of Thrones Companion APP',
-            magnet: 'magnet:?xt=urn:btih:B170691FE3B6AAEBA73F8EA122E85DED4AD2E1BF&dn=game+of+thrones+s04e04+hdtv+x264+killers+ettv&tr=http%3A%2F%2Ftracker.trackerfix.com%2Fannounce&tr=udp%3A%2F%2Fopen.demonii.com%3A1337',
-            size: '213 MB',
-            age: '1 year',
-            seed: 45,
-            leech: 42,
-            category: 'app',
-            color: getColor('app'),
-            icon: getIcon('app')
-        }, {
-            id: 7,
-            name: 'Game of Thrones - XXX Parody',
-            magnet: 'magnet:?xt=urn:btih:B170691FE3B6AAEBA73F8EA122E85DED4AD2E1BF&dn=game+of+thrones+s04e04+hdtv+x264+killers+ettv&tr=http%3A%2F%2Ftracker.trackerfix.com%2Fannounce&tr=udp%3A%2F%2Fopen.demonii.com%3A1337',
-            size: '678 MB',
-            age: '1 year',
-            seed: 866,
-            leech: 123,
-            category: 'xxx',
-            color: getColor('xxx'),
-            icon: getIcon('xxx')
-        }];
+        var torrents = [];
 
         return {
             all: function () {
@@ -187,20 +99,47 @@ angular.module('magnetizr.services', [])
             },
             get: function (torrentId) {
                 for (var i = 0; i < torrents.length; i++) {
-                    if (torrents[i].id === parseInt(torrentId)) {
+                    if (torrents[i].id === torrentId) {
                         return torrents[i];
                     }
                 }
                 return null;
             },
             search: function (query) {
-                var result = [];
-                for (var i = 0; i < torrents.length; i++) {
-                    if (torrents[i].name.search(new RegExp(query, 'i')) >= 0) {
-                        result.push(torrents[i]);
-                    }
-                }
-                return result;
+                $http.get('https://getstrike.net/api/v2/torrents/search/?phrase=' + query).
+                    success(function(data, status, headers, config) {
+                        torrents.length = 0;
+
+                        var torrentItems = data.torrents;
+                        console.log(torrentItems);
+
+                        for (var i = 0; i < torrentItems.length; i++) {
+                            var torrent = torrentItems[i];
+                            torrents.push({
+                                id: torrent.torrent_hash,
+                                name: torrent.torrent_title,
+                                magnet: torrent.magnet_uri,
+                                size: torrent.size,
+                                age: torrent.upload_date,
+                                seed: torrent.seeds,
+                                leech: torrent.leeches,
+                                category: torrent.torrent_category,
+                                color: getColor(torrent.torrent_category),
+                                icon: getIcon(torrent.torrent_category)
+                            });
+                        }
+
+                        console.log("Results:");
+                        console.log(torrents);
+
+                        return torrents;
+
+                    }).
+                    error(function(data, status, headers, config) {
+                        alert("Something went wrong");
+                    });
+
+                return torrents;
             },
             download: function (torrent) {
                 navigator.app.loadUrl(torrent.magnet, {openExternal: true});
