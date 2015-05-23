96a84729617d027fa94652e969578feb2b1bc93d
ipinto <ipintoc@gmail.com>
2015-05-23 18:42:46 +0200

    Modified plugins script so it can be executed

--- plugins.sh
+++ plugins.sh
@@ -1,3 +1,6 @@
+#!/bin/sh
+# Script que elimina y añade todos los plugins necesarios
+
 cordova plugin remove cordova-plugin-globalization
 cordova plugin add https://git-wip-us.apache.org/repos/asf/cordova-plugin-globalization.git
 
