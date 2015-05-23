7eff104d1b606fecce78c5792a8dd6d3d1b6e7ec
ipinto <ipintoc@gmail.com>
2015-05-23 22:19:49 +0200

    Added globalization plugin to ionic state config file

--- plugins.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# Script que elimina y añade todos los plugins necesarios
-
-cordova plugin remove cordova-plugin-globalization
-cordova plugin add https://git-wip-us.apache.org/repos/asf/cordova-plugin-globalization.git
-
